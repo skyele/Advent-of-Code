a86d280b8def5d136057d4e4bd0b578803478838
skyx <2286455782@qq.com>
2024-12-03 11:42:42 +0800

    day 2 - 2

--- 2024/solutions/src/day2.rs
+++ 2024/solutions/src/day2.rs
@@ -46,19 +46,47 @@ pub fn is_safe(vec: &Vec<i32>) -> bool {
   return is_safe_metric1(vec) && is_safe_metric2(vec);
 }
 
-pub fn solve_1() {
-  let lines = read_lines("inputs/day2.txt").unwrap();
+pub fn is_same_with_dampener(vec: &Vec<i32>) -> bool {
+  if is_safe_metric1(vec) && is_safe_metric2(vec) {
+    return true;
+  }
+
+  for i in 0..vec.len() {
+    let mut new_vec = vec.clone();
+    new_vec.remove(i);
+    if is_safe_metric1(&new_vec) && is_safe_metric2(&new_vec) {
+      return true;
+    }
+  }
+  return false;
+}
+
+pub fn parse_input(fname: &str) -> Vec<Vec<i32>> {
+  let lines = read_lines(fname).unwrap();
   let mut vec_2d: Vec<Vec<i32>> = Vec::new();
   for line in lines{
     let local_vec: Vec<i32> = line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
     vec_2d.push(local_vec);
   }
+  return vec_2d;
+}
+
+pub fn solve_1() {
+  let vec_2d = parse_input("inputs/day2.txt");
 
   let mut safe_cnt = 0;
   for vec in vec_2d {
-    if is_safe(&vec) {
-      safe_cnt += 1;
-    }
+    safe_cnt += is_safe(&vec) as i32;
   }
   println!("Safe count: {}", safe_cnt);
+}
+
+pub fn solve_2() {
+  let vec_2d = parse_input("inputs/day2.txt");
+
+  let mut safe_cnt = 0;
+  for vec in vec_2d {
+    safe_cnt += is_same_with_dampener(&vec) as i32;
+  }
+  println!("Safe count with dampener: {}", safe_cnt);
 }
--- 2024/solutions/src/main.rs
+++ 2024/solutions/src/main.rs
@@ -1,8 +1,11 @@
 mod common;
 mod day1_1;
 mod day1_2;
+mod day2;
 
 fn main() {
-    day1_1::solve();
-    day1_2::solve();
+    // day1_1::solve();
+    // day1_2::solve();
+    day2::solve_1();
+    day2::solve_2();
 }
