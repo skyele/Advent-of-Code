0d749f0b50211b84728a61754f51e5682b7fe1c8
skyx <2286455782@qq.com>
2024-12-11 23:40:54 +0800

    day 11 - 2

--- 2024/solutions/src/day11.rs
+++ 2024/solutions/src/day11.rs
@@ -1,55 +1,104 @@
 use crate::common::file_helper::read_lines;
 use crate::common::math_helper;
+use std::collections::HashMap;
 
-pub fn blink(nums: &mut Vec<i64>) {
-    let mut to_insert: Vec<(usize, i64)> = Vec::new();
-    for (idx, ele) in nums.into_iter().enumerate() {
-        if ele == &0 {
-            *ele = 1;
+pub fn process_new_num(
+    num: i64,
+    cnt: i64,
+    new_num_cnt: &mut HashMap<i64, i64>,
+    num_cache: &mut HashMap<i64, Vec<i64>>,
+) {
+    let (mut num1, mut num2) = (Option::None, Option::None);
+    if num == 0 {
+        num1 = Some(1);
+    } else {
+        let num_digits = math_helper::get_num_digits(num);
+        if num_digits % 2 == 0 {
+            let mask = 10i64.pow((num_digits / 2) as u32);
+            num1 = Some(num / mask);
+            num2 = Some(num % mask);
         } else {
-            let num_digits = math_helper::get_num_digits(*ele);
-            if num_digits % 2 == 0 {
-                let mask = 10i64.pow((num_digits / 2) as u32);
-                let right = *ele % mask;
-                let left = *ele / mask;
-                *ele = left;
-                to_insert.push((idx + 1, right));
-            } else {
-                *ele = (*ele).checked_mul(2024).unwrap();
-            }
+            num1 = Some(num.checked_mul(2024).unwrap());
         }
     }
 
-    for (idx, ele) in to_insert.into_iter().rev() {
-        nums.insert(idx, ele);
+    for &ele in [&num1, &num2] {
+        ele.map(|ele| {
+            new_num_cnt
+                .entry(ele)
+                .and_modify(|v| *v = v.checked_add(cnt).unwrap())
+                .or_insert(cnt);
+            num_cache.entry(num).or_insert(Vec::new()).push(ele);
+        });
     }
 }
 
-pub fn parse_stones(line: &str) -> Vec<i64> {
+pub fn process_exist_num(
+    num: i64,
+    cnt: i64,
+    new_num_cnt: &mut HashMap<i64, i64>,
+    num_cache: &mut HashMap<i64, Vec<i64>>,
+) {
+    num_cache.get(&num).unwrap().iter().for_each(|&ele| {
+        new_num_cnt
+            .entry(ele)
+            .and_modify(|v| *v = v.checked_add(cnt).unwrap())
+            .or_insert(cnt);
+    });
+}
+
+pub fn blink(num_cnt: &mut HashMap<i64, i64>, num_cache: &mut HashMap<i64, Vec<i64>>) {
+    let mut new_num_cnt: HashMap<i64, i64> = HashMap::new();
+
+    for (&num, &cnt) in num_cnt.iter() {
+        if !num_cache.contains_key(&num) {
+            process_new_num(num, cnt, &mut new_num_cnt, num_cache);
+        } else {
+            process_exist_num(num, cnt, &mut new_num_cnt, num_cache);
+        }
+    }
+
+    *num_cnt = new_num_cnt;
+}
+
+pub fn parse_stones(line: &str) -> HashMap<i64, i64> {
     return line
-        .split(" ")
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(|x| x.parse().unwrap())
-        .collect();
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().expect("Failed to parse number"))
+        .fold(HashMap::new(), |mut acc, num| {
+            *acc.entry(num).or_insert(0) += 1;
+            acc
+        });
 }
 
-pub fn print_stones(stones: &Vec<i64>) {
-    for stone in stones {
-        print!("{} ", stone);
-    }
-    println!();
+pub fn score_num_cnt(num_cnt: &HashMap<i64, i64>) -> i64 {
+    return num_cnt.into_iter().fold(0, |acc, (_, cnt)| acc + cnt);
 }
 
 pub fn solve_1() -> i64 {
     let lines = read_lines("inputs/day11.txt").unwrap();
-    let mut nums: Vec<i64> = parse_stones(&lines[0]);
+    let mut num_cnt: HashMap<i64, i64> = parse_stones(&lines[0]);
+    let mut num_cache: HashMap<i64, Vec<i64>> = HashMap::new();
 
     for i in 0..25 {
-        blink(&mut nums);
+        blink(&mut num_cnt, &mut num_cache);
     }
 
-    let res = nums.len() as i64;
+    let res = score_num_cnt(&num_cnt);
+    println!("res={}", res);
+    return res;
+}
+
+pub fn solve_2() -> i64 {
+    let lines = read_lines("inputs/day11.txt").unwrap();
+    let mut num_cnt: HashMap<i64, i64> = parse_stones(&lines[0]);
+    let mut num_cache: HashMap<i64, Vec<i64>> = HashMap::new();
+
+    for i in 0..75 {
+        blink(&mut num_cnt, &mut num_cache);
+    }
+
+    let res = score_num_cnt(&num_cnt);
     println!("res={}", res);
     return res;
 }
